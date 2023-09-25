7240020823a6cd36fd07c34a30868a8ccc95c5ab
Aryan Kesharwani <aryan.kesharwani@nagarro.com>
2023-09-25 14:58:13 +0530

    Second committ

--- javacode.java
+++ javacode.java
@@ -3,5 +3,9 @@ class javacode {
 public static void main(String args[])  //static method  
 {  
 System.out.println("Static method");  
+helloWord();
 }  
+public static void helloWord(){
+   System.out.println("HelloWorld");  
+}
 }
